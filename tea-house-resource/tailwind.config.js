2584656d9477b8bff6645287ef0960e52b5640df
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-08-08 23:58:44 +0600

    html-project

---
+++ tea-house-resource/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      color: "#1C1C1C",
+    },
+    plugins: [],
+  }
